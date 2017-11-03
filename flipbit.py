3e697312831d785e77e3ce546432591d7cbbbddb
cairdcoinheringaahing <valyrio.email@gmail.com>
2017-11-03 18:11:13 +0000

    Added debug flags

--- flipbit.py
+++ flipbit.py
@@ -30,7 +30,7 @@ def nth_index(string, substring, N, reverse):
 				return i
 	return -1
 
-def run(code, inputs, debug):
+def run(code, inputs, debug, super_debug):
 	stream = list(to_binary(*inputs))
 	pindex = 0
 	iindex = 0
@@ -54,21 +54,27 @@ def run(code, inputs, debug):
 				tape[tindex] = 0
 		if char == '.':
 			num = tape[:tindex+1]
-			print(end=from_binary(num))
+			print(end=from_binary(num)+'\n'*super_debug)
 		if char == '[':
 			loop_depth += 1
-			if tape[tindex]:
+			if not tape[tindex]:
 				pindex = nth_index(code, ']', loop_depth, -1)
 		if char == ']':
-			if not tape[tindex]:
+			if tape[tindex]:
 				pindex = nth_index(code, '[', loop_depth, 1)
 			else:
 				loop_depth -= 1
+		if char == '#' and debug:
+			index = ''.join(map(str, tape)).rindex('1') if (''.join(map(str, tape)).rindex('1') + 1) > tindex else tindex
+			if debug:
+				print('\n', tape[:index+1], sep='')
 		pindex += 1
+		if super_debug:
+			print(('%s %-{}s %-2s %s').format(len(str(len(code)))) % (char, pindex, tindex, tape[:tindex+1]))
 
 	index = ''.join(map(str, tape)).rindex('1') if (''.join(map(str, tape)).rindex('1') + 1) > tindex else tindex
 	if debug:
 		print('\n', tape[:index+1], sep='')
 
 if __name__ == '__main__':
-	run(sys.argv[1], sys.stdin.read(), '-d' in sys.argv[2:])
+	run(sys.argv[1], sys.stdin.read(), '-d' in sys.argv[2:], '--debug' in sys.argv[2:])
